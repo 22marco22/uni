7b380c375f4ef8283deb161468d98033c13480b1
marco lauria <150843558+22marco22@users.noreply.github.com>
2024-11-13 20:03:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ var resumeStudyingLink=document.querySelector('a[href^="https://lms-courses.merc
 if (resumeStudyingLink) {
    resumeStudyingLink.click();
 }
+var checkLoaded=setInterval(function(){
+   if(document.readyState === 'complete'){
+      //code here
+      clearInterval(checkLoaded);
+   }
+},1000); 
