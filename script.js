9ce4d555dce963caeaf3b93fd0484047dce451f1
marco lauria <150843558+22marco22@users.noreply.github.com>
2024-11-13 19:58:59 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+var resumeStudyingLink=document.querySelector('a[href^="https://lms-courses.mercatorum.multiversity.click/efs-courses"]');     
+if (resumeStudyingLink) {
+   resumeStudyingLink.click();
+}
